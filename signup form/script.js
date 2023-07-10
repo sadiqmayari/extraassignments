84640aad31802e5d4405ee0651cf02b7e6771c90
sadiqmayari <sadiqmayari@gmail.com>
2023-07-10 16:37:07 +0500

    update

--- signup form/script.js
+++ signup form/script.js
@@ -1,14 +1,18 @@
 const submitBtn = document.getElementById('submitBtn');
 const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+const passError = document.getElementById('passError');
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    if (validateName() && validateEmail == true) {
+    if(validateName() && validateEmail() && validatePassword()) {
         alert("Form Submitted Successfully");
     }
 });
 
+
+//  validate name
 function validateName(){
     let name = document.getElementById('name').value;
 
@@ -17,7 +21,8 @@ function validateName(){
         nameError.previousElementSibling.classList.add('fa-xmark');
         return false;
     }
-    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
         nameError.innerHTML = "Write Full Name";
         nameError.previousElementSibling.classList.add('fa-xmark');
         return false;
@@ -27,21 +32,46 @@ function validateName(){
     return true;
 }
 
-function validateEmail(){
+
+// validate email
+function validateEmail() {
     let email = document.getElementById('email').value;
 
 
-    if (email.length == 0) {
+    if (email.length == 0){
         emailError.innerHTML = "Email is required";
         emailError.previousElementSibling.classList.add('fa-xmark');
         return false;
     }
-    if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+
+    if(!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
         emailError.innerHTML = "Enter Valid Email Address";
         emailError.previousElementSibling.classList.add('fa-xmark');
         return false;
     }
     emailError.innerHTML = "";
     emailError.previousElementSibling.classList.add('fa-check');
+    return true;
+}
+
+
+// validate pawssword
+function validatePassword() {
+    let password = document.getElementById('password').value;
+
+
+    if (password.length == 0) {
+        passError.innerHTML = "Password is required";
+        passError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+
+    if(!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,30}$/)){
+        passError.innerHTML = "Password Must be Multi Characters";
+        passError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+    passError.innerHTML = "";
+    passError.previousElementSibling.classList.add('fa-check');
     return true;
 }
